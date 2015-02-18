6673eccce319fc1eb7639db294d8925b7df85a02
Fabio Benigno <benigno.fabio@gmail.com>
2015-02-17 22:08:19 -0400

    schema file not a symlink, previous commit fixed
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

--- python/persistence.py
+++ python/persistence.py
@@ -27,24 +27,27 @@ class Persistence:
     def get_message_from_file(path):
         if (Utils.is_backup_file(path)):
             return None
+        file_name = Utils.extract_file_name(path)
+        parent_folder = Utils.extract_file_parent_folder(path)
+        message = Message(parent_folder + "_" + file_name)
+        message.add_field(MESSAGE_TEXT_FIELD_NAME, EMPTY_STRING)
         try:
             with open(path, READ_MODE) as archive:
-                file_name = Utils.extract_file_name(path)
-                parent_folder = Utils.extract_file_parent_folder(path)
-                message = Message(parent_folder + "_" + file_name)
-                message.add_field(MESSAGE_TEXT_FIELD_NAME, EMPTY_STRING)
                 for line in archive:
                     line = line.translate(CONTROL_CHARS_TO_IGNORE)
                     separator_index = Utils.string_find(NEWSGROUPS_CORPORA_FIELD_SEPARATOR, line)
                     line_has_separator = separator_index is not None
-                    field_name_is_valid = Utils.validate_field_name(line[0:separator_index])
-                    field_name_is_new = not message.has_field(line[0:separator_index])
-                    is_line_field = line[0:separator_index] == MESSAGE_LINE_FIELD_NAME
-                    is_not_integer = not Utils.is_integer(line[0:separator_index])
-                    if (line_has_separator and field_name_is_valid and field_name_is_new and not (is_line_field and is_not_integer)):
-                        field_name = line[0:separator_index]
-                        field_value = line[separator_index + 1:]
-                        field_value = field_value.rstrip(EOL).strip()
+                    if (not line_has_separator):
+                        message.append_field(MESSAGE_TEXT_FIELD_NAME, line)
+                        continue
+                    field_name = line[0:separator_index]
+                    field_value = line[separator_index + 1:].rstrip(EOL).strip()
+                    field_name_is_valid = Utils.validate_field_name(field_name)
+                    field_name_is_new = not message.has_field(field_name)
+                    is_line_field = field_name == MESSAGE_LINE_FIELD_NAME
+                    is_not_integer = not Utils.is_integer(field_value)
+                    is_invalid_line_field = is_line_field and is_not_integer
+                    if (field_name_is_valid and field_name_is_new and not is_invalid_line_field):
                         message.add_field(field_name, field_value)
                     else:
                         message.append_field(MESSAGE_TEXT_FIELD_NAME, line)
