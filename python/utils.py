285c517fa327244fe30513137281d82d58b90a20
Fabio Benigno <benigno.fabio@gmail.com>
2015-02-18 20:26:03 -0400

    Improved backup file detection
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

--- python/utils.py
+++ python/utils.py
@@ -57,10 +57,27 @@ class Utils:
         return name
 
     @staticmethod
+    def extract_file_extension(path):
+        separator_index = None
+        file_extension = None
+        for i in range(len(path) - 1, -1, -1):
+            if (path[i] == LINUX_PATH_SEPARATOR):
+                break
+            if (path[i] == FILE_EXTENSION_SEPARATOR):
+                separator_index = i
+        if (separator_index is not None):
+            file_extension = path[separator_index:]
+        return file_extension
+
+    @staticmethod
     def is_backup_file(path):
-        if (BACKUP_SYMBOL in path):
-            return True
-        return False
+        file_name = Utils.extract_file_name(path)
+        file_extension = Utils.extract_file_extension(path)
+        if (file_extension is not None):
+            last_char_index = len(file_extension) - 1
+            return file_extension[last_char_index] == BACKUP_SYMBOL
+        last_char_index = len(file_name) - 1
+        return file_name[last_char_index] == BACKUP_SYMBOL
 
     @staticmethod
     def validate_field_name(string):
