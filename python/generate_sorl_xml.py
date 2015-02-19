e9541dbd1959b7a2ad1ee9145d3168c5898fe204
Fabio Benigno <benigno.fabio@gmail.com>
2015-02-18 21:41:15 -0400

    Fix wrong naming in generate...py
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

--- python/generate_sorl_xml.py
+++ python/generate_sorl_xml.py
@@ -2,11 +2,11 @@
 
 
 import sys
-from message import Message
+from document import Document
 from persistence import Persistence
 from const import XML_FILE_EXTENSION
 
 
-message = Persistence.get_message_from_file(sys.argv[1])
-if (message is not None):
-    Persistence.message_to_solr_xml(message, sys.argv[2] + message.identifier + XML_FILE_EXTENSION)
+document = Persistence.get_document_from_file(sys.argv[1])
+if (document is not None):
+    Persistence.document_to_solr_xml(document, sys.argv[2] + document.identifier + XML_FILE_EXTENSION)
